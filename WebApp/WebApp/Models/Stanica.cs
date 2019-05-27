699743c5f02b75a5d1955017bb30559205835a73
marijavukomanovic <mariv996@gmail.com>
2019-05-27 20:41:00 +0200

    Dodata-stanica i linija

---
+++ WebApp/WebApp/Models/Stanica.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class Stanica
+    { [Key]
+        public String Naziv { get; set; }
+        public String Adresa { get; set; }
+        public int GeografskeKoordinate { get; set; }
+        public List<Linija> Linije { get; set; }
+
+    }
+}
