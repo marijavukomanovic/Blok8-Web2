0240b0e983dd8d74ebdb4c95dfad428786174e38
Kima7 <marinadimitrijevic07@gmail.com>
2019-06-03 13:10:39 +0200

    loginCompom

---
+++ klijent/appProba/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import {Routes} from '@angular/router';
+import { LogInComponent } from './log-in/log-in.component';
+
+const routes : Routes =[
+  {path:'',component:HomeComponent},
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LogInComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
